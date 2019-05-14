4c3fad759039b528c8703562c7b09f4fb854b695
Julian <48802692+Lujen@users.noreply.github.com>
2019-05-14 20:45:09 +0200

    Add files via upload

---
+++ Zegar/Zegar/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Zegar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.Time = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Blue;
+            this.button1.FlatAppearance.BorderSize = 2;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(328, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(160, 37);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start the clock";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Time
+            // 
+            this.Time.BackColor = System.Drawing.Color.Transparent;
+            this.Time.Font = new System.Drawing.Font("Microsoft Sans Serif", 26F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Time.Location = new System.Drawing.Point(328, 404);
+            this.Time.Name = "Time";
+            this.Time.Size = new System.Drawing.Size(160, 37);
+            this.Time.TabIndex = 1;
+            this.Time.Text = "Time";
+            this.Time.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SlateGray;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Time);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label Time;
+    }
+}
+
---
+++ Zegar/Zegar/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zegar
+{   struct vector2
+    {
+        public int x;
+        public int y;
+        public vector2(int x,int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+    public partial class Form1 : Form
+    {
+        
+        vector2 LeftUpperClock;
+        vector2 CenterOfTheClock;
+
+        vector2 SecondPointer; // wektor sekundowy
+        vector2 HourPointer; // wektor godziny
+        vector2 MinutePointer; // wektor minuty
+
+        Graphics g; // używanie grafiki
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            g = this.CreateGraphics();
+
+            g.DrawRectangle(new Pen(Color.Silver, 6), LeftUpperClock.x-6, LeftUpperClock.y-6, 312, 312);
+            g.DrawEllipse(new Pen(Color.DarkRed, 6), LeftUpperClock.x, LeftUpperClock.y, 300, 300);
+            g.DrawString("12", new Font("Arial", 26, FontStyle.Bold), Brushes.Black, new Point(ActiveForm.Width / 2 - 25, ActiveForm.Height / 2 - 140));
+            g.DrawString("6", new Font("Arial", 26, FontStyle.Bold), Brushes.Black, new Point(ActiveForm.Width / 2 - 16, ActiveForm.Height / 2 + 110));
+            g.DrawString("3", new Font("Arial", 26, FontStyle.Bold), Brushes.Black, new Point(ActiveForm.Width / 2 + 110, ActiveForm.Height / 2 - 19));
+            g.DrawString("9", new Font("Arial", 26, FontStyle.Bold), Brushes.Black, new Point(ActiveForm.Width / 2 - 140, ActiveForm.Height / 2 - 19));
+
+
+            DateTime teraz = DateTime.Now;
+
+            g.DrawLine(new Pen(Color.SlateGray, 3), CenterOfTheClock.x, CenterOfTheClock.y, SecondPointer.x, SecondPointer.y);// usuwanie poprzedniej wskazówki sekund
+
+            if (teraz.Second == 0.000f)
+            {
+                g.DrawLine(new Pen(Color.SlateGray, 4), CenterOfTheClock.x, CenterOfTheClock.y, MinutePointer.x, MinutePointer.y);
+                // usuwanie poprzedniej wskazówki minut co 60 sekund(sekundy wchodzą w cykl nowej minuty)
+            }
+            if (teraz.Minute == 0.0f)
+            {
+                g.DrawLine(new Pen(Color.SlateGray, 5), CenterOfTheClock.x, CenterOfTheClock.y, HourPointer.x, HourPointer.y);
+                // usuwanie poprzedniej wskazówki godziny co 60 minut(kiedy minuty sie wyzerują i wejda w cykl nowej godziny)
+            }
+            //sekundy
+
+            SecondPointer.x = CenterOfTheClock.x + (int)(140 * Math.Sin(Math.PI * teraz.Second*6 / 180));
+            SecondPointer.y = CenterOfTheClock.y - (int)(140 * Math.Cos(Math.PI * teraz.Second*6/ 180));
+
+            g.DrawLine(new Pen(Color.Black, 3), CenterOfTheClock.x, CenterOfTheClock.y, SecondPointer.x, SecondPointer.y);//rysowanie wskazówki
+
+            //minuty
+            
+            MinutePointer.x = CenterOfTheClock.x + (int)(120 * Math.Sin(Math.PI * teraz.Minute * 6 / 180));
+            MinutePointer.y = CenterOfTheClock.y - (int)(120 * Math.Cos(Math.PI * teraz.Minute * 6 / 180));
+
+            g.DrawLine(new Pen(Color.Black, 4), CenterOfTheClock.x, CenterOfTheClock.y, MinutePointer.x, MinutePointer.y);//rysowanie wskazówki
+            // godziny
+            
+            HourPointer.x = CenterOfTheClock.x + (int)(100 * Math.Sin(Math.PI * teraz.Hour * 30 / 180));
+            HourPointer.y = CenterOfTheClock.y - (int)(100 * Math.Cos(Math.PI * teraz.Hour * 30 / 180));
+
+            g.DrawLine(new Pen(Color.Black, 5), CenterOfTheClock.x, CenterOfTheClock.y, HourPointer.x, HourPointer.y);//rysowanie wskazówki
+
+            Time.Text = teraz.Hour + ":" + teraz.Minute + ":" + teraz.Second;
+            g.Dispose();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            CenterOfTheClock = new vector2(ActiveForm.Width/2, ActiveForm.Height/2);           
+            LeftUpperClock = new vector2((ActiveForm.Width / 2)-150, (ActiveForm.Height / 2)-150);
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+             
+        }
+    }
+}
